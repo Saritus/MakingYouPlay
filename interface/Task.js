4d3c86783b1f3cddebb8e9f21497980ab22f2a8f
Sebastian Mischke <SebastianMischke@gmx.de>
2018-05-29 22:24:01 +0200

    Change start text

--- interface/Task.js
+++ interface/Task.js
@@ -3,7 +3,7 @@ function Task(x, y, w, h) {
   this.y = y;
   this.w = w;
   this.h = h;
-  this.message = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et.";
+  this.message = Math.random();
 
   this.show = function() {
     textAlign("center");
