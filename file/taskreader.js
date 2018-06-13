617651c56e8fd384bc23b71d95c06fc4d77d45cb
Sebastian Mischke <SebastianMischke@gmx.de>
2018-06-13 22:27:02 +0200

    Extract evaluate lines to new function

--- file/taskreader.js
+++ file/taskreader.js
@@ -13,26 +13,30 @@ function TaskReader() {
         taskCount = 0;
         // Split text based on lines
         var lines = reader.result.split('\n');
-        // Iterate through all lines
-        for (var index = 0; index < lines.length; index++) {
-          let line = lines[index];
-          if (line.length < 2) {
-            // Ignore this line
-          } else if (line.includes(';')) {
-            let parts = line.split(';');
-            let chance = parseFloat(parts[1].replace(',', '.'));
-            let occurrences = parts.length > 2 ? parseInt(parts[2]) : -1;
-            tasks.push([parts[0], chance, occurrences]);
-            taskCount += chance;
-          } else {
-            tasks.push([line, 1, -1]);
-            taskCount += 1;
-          }
-        }
+        this.evaluateLines(lines);
         // Resolve promise
         resolve();
-      }
+      }.bind(this)
       reader.readAsText(file, 'windows-1252');
-    });
+    }.bind(this));
+  }
+
+  this.evaluateLines = function(lines) {
+    // Iterate through all lines
+    for (var index = 0; index < lines.length; index++) {
+      let line = lines[index];
+      if (line.length < 2) {
+        // Ignore this line
+      } else if (line.includes(';')) {
+        let parts = line.split(';');
+        let chance = parseFloat(parts[1].replace(',', '.'));
+        let occurrences = parts.length > 2 ? parseInt(parts[2]) : -1;
+        tasks.push([parts[0], chance, occurrences]);
+        taskCount += chance;
+      } else {
+        tasks.push([line, 1, -1]);
+        taskCount += 1;
+      }
+    }
   }
 }
